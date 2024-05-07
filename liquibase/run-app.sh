a3149ac4a04bd395c8fb49d8dcc9da0b00528343
Amogh Shetkar <ashetkar@yugabyte.com>
2024-05-07 13:19:43 +0530

    Change Java version

--- liquibase/run-app.sh
+++ liquibase/run-app.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Cloning the liquibase extension repository"
 
-git clone git@github.com:liquibase/liquibase-yugabytedb.git && cd liquibase-yugabytedb
+git clone -q git@github.com:liquibase/liquibase-yugabytedb.git && cd liquibase-yugabytedb
 $YUGABYTE_HOME_DIRECTORY/bin/ysqlsh -f ./src/test/resources/docker/yugabytedb-init.sql
 
 echo "Editing the config file"
@@ -12,7 +12,7 @@ rm src/test/resources/harness-config.yml && cp $INTEGRATIONS_HOME_DIRECTORY/liqu
 
 echo "Running tests"
 
-mvn clean install
-mvn -ntp -Dtest=FoundationalExtensionHarnessSuite test > $ARTIFACTS_PATH/liquibasefoundationaltest.txt
+JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn clean install
+JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp -Dtest=FoundationalExtensionHarnessSuite test > $ARTIFACTS_PATH/liquibasefoundationaltest.txt
 
 ! grep "BUILD FAILURE" $ARTIFACTS_PATH/liquibasefoundationaltest.txt
