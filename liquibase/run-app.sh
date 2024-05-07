2034a7422ddfc8147ab2ddceffb816a2938b7cbb
Amogh Shetkar <ashetkar@yugabyte.com>
2024-05-07 18:34:29 +0530

    Replace YUGABYTE_RELEASE_NUMBER with actual value

--- liquibase/run-app.sh
+++ liquibase/run-app.sh
@@ -9,10 +9,16 @@ $YUGABYTE_HOME_DIRECTORY/bin/ysqlsh -f ./src/test/resources/docker/yugabytedb-in
 echo "Editing the config file"
 
 rm src/test/resources/harness-config.yml && cp $INTEGRATIONS_HOME_DIRECTORY/liquibase/harness-config.yml src/test/resources
+sed -i '' 's@${YUGABYTE_RELEASE_NUMBER}@'"$YUGABYTE_RELEASE_NUMBER"'@' src/test/resources/harness-config.yml
 
 echo "Building the Liquibase tests"
 JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp -q clean install
 echo "Running the Liquibase tests"
-JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp -Dtest=FoundationalExtensionHarnessSuite test
+JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp -Dtest=FoundationalExtensionHarnessSuite test > $ARTIFACTS_PATH/liquibasefoundationaltest.txt
+
+if [ $(grep -c "BUILD SUCCESS" $ARTIFACTS_PATH/liquibasefoundationaltest.txt) -eq 0 ]
+then
+  cat $ARTIFACTS_PATH/liquibasefoundationaltest.txt
+fi
 
 ! grep "BUILD FAILURE" $ARTIFACTS_PATH/liquibasefoundationaltest.txt
