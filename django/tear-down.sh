4fd71edd407d75347417379cc27a7ffe579f9337
Sfurti-yb <78196231+Sfurti-yb@users.noreply.github.com>
2024-05-10 22:48:17 +0530

    Replaced the entire async module of Django tests with only the classes that pass (#15)
    * Replaced the entire async module with only the classes that pass
    * Removing async.test_async_auth as it is only present in v5.0.x of django

--- django/tear-down.sh
+++ django/tear-down.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-deactivate
-rm -rf $WORKSPACE/environments/django-test
-rm -rf $WORKSPACE/yb-django
-rm -rf $WORKSPACE/$DJANGO_TESTS_DIR
+export DJANGO_TESTS_DIR="django_tests_dir"
 
 # Destroy YugabyteDB cluster
 $YUGABYTE_HOME_DIRECTORY/bin/yb-ctl destroy
+
+rm -rf $WORKSPACE/environments/django-test
+rm -rf $INTEGRATIONS_HOME_DIRECTORY/django/yb-django
+rm -rf $INTEGRATIONS_HOME_DIRECTORY/django/$DJANGO_TESTS_DIR
