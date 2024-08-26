db56a3866f089faac487cb92d9835a449f447289
Harsh Daryani <harsh.daryani.6211@gmail.com>
2024-08-26 07:17:56 +0000

    Printing test output to console 2

--- pgx/run-app.sh
+++ pgx/run-app.sh
@@ -25,15 +25,15 @@ go build ybsql_load_balance.go ybsql_load_balance_pool.go ybsql_fallback.go perf
 
 echo "Running tests"
 
-./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY > $ARTIFACTS_PATH/pgx_connect.txt 2>&1
+./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY 2>&1 | tee $ARTIFACTS_PATH/pgx_connect.txt
 
-./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --pool > $ARTIFACTS_PATH/pgxpool_connect.txt 2>&1
+./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --pool 2>&1 | tee $ARTIFACTS_PATH/pgxpool_connect.txt
 
-./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --fallbackTest 1 > $ARTIFACTS_PATH/pgx_fallback1.txt 2>&1
+./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --fallbackTest 1 2>&1 | tee $ARTIFACTS_PATH/pgx_fallback1.txt
 
-./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --fallbackTest 2 > $ARTIFACTS_PATH/pgx_fallback2.txt 2>&1
+./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --fallbackTest 2 2>&1 | tee $ARTIFACTS_PATH/pgx_fallback2.txt
 
-./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --fallbackTest 3 > $ARTIFACTS_PATH/pgx_fallback3.txt 2>&1
+./ybsql_load_balance $YUGABYTE_HOME_DIRECTORY --fallbackTest 3 2>&1 | tee $ARTIFACTS_PATH/pgx_fallback3.txt
 
 grep "Closing the application ..." $ARTIFACTS_PATH/pgx_connect.txt
 
