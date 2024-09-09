6786b9d53b49960424ae722d5f8941b81820d695
Amogh Shetkar <ashetkar@yugabyte.com>
2024-09-09 18:04:22 +0530

    Include read-replica jdbc tests

--- ysql-jdbc/do-start.sh
+++ ysql-jdbc/do-start.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Launch YugabyteDB
-# printf "Executing start-ybdb.sh ...\n"
-# ./start-ybdb.sh
-
 # Start the test/example application and generate reports
 printf "Executing run-app.sh ...\n"
 ./run-app.sh
