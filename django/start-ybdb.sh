71f502f233e12b0d9b39571b7d655d11289c00b0
Manav Kumar <mkumar@yugabyte.com>
2024-09-12 07:09:13 +0000

    Fix the quotes for conn mgr flags in django and liquibase

--- django/start-ybdb.sh
+++ django/start-ybdb.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Start YugabyteDB
-$YUGABYTE_HOME_DIRECTORY/bin/yb-ctl create --tserver_flags=\'enable_ysql_conn_mgr=true,\"allowed_preview_flags_csv=enable_ysql_conn_mgr\"\'
+$YUGABYTE_HOME_DIRECTORY/bin/yb-ctl create --tserver_flags='enable_ysql_conn_mgr=true,"allowed_preview_flags_csv=enable_ysql_conn_mgr"'
--- liquibase/start-ybdb.sh
+++ liquibase/start-ybdb.sh
@@ -5,4 +5,4 @@ set -e
 $YUGABYTE_HOME_DIRECTORY/bin/yb-ctl destroy
 
 # Start a new YugabyteDB cluster
-$YUGABYTE_HOME_DIRECTORY/bin/yb-ctl create --tserver_flags=\'enable_ysql_conn_mgr=true,\"allowed_preview_flags_csv=enable_ysql_conn_mgr\"\'
+$YUGABYTE_HOME_DIRECTORY/bin/yb-ctl create --tserver_flags='enable_ysql_conn_mgr=true,"allowed_preview_flags_csv=enable_ysql_conn_mgr"'
