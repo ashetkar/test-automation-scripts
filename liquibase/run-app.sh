ea9a78198693dcf4d2b1e26bb9129803260c30ab
Sfurti-yb <sfurtisarah@gmail.com>
2024-10-30 10:00:03 +0530

    generate liquibase report

--- liquibase/run-app.sh
+++ liquibase/run-app.sh
@@ -10,29 +10,19 @@ $YUGABYTE_HOME_DIRECTORY/bin/ysqlsh -f ./src/test/resources/docker/yugabytedb-in
 echo "Editing the config file"
 
 rm src/test/resources/harness-config.yml && cp $INTEGRATIONS_HOME_DIRECTORY/liquibase/harness-config.yml src/test/resources
-sed -i 's@${YUGABYTE_RELEASE_NUMBER}@'"$YUGABYTE_RELEASE_NUMBER"'@' src/test/resources/harness-config.yml
+
 
 echo "Building the Liquibase tests"
-JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp -q clean install
+mvn -ntp -q clean install
 echo "Running the Liquibase tests"
-JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp test 2>&1 | tee $ARTIFACTS_PATH/liquibase_yugabytedb_test_report.txt
-JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp -Dtest=FoundationalExtensionHarnessSuite test 2>&1 | tee $ARTIFACTS_PATH/liquibase_foundational_test_report.txt
-JAVA_HOME=/usr/lib/jvm/zulu-11.jdk mvn -ntp -Dtest=AdvancedExtensionHarnessSuite test 2>&1 | tee $ARTIFACTS_PATH/liquibase_advanced_test_report.txt
+mvn -ntp test 2>&1 | tee $ARTIFACTS_PATH/liquibase_yugabytedb_test_report.txt
 
 echo "Checking the Liquibase test reports"
 if [ $(grep -c "BUILD SUCCESS" $ARTIFACTS_PATH/liquibase_yugabytedb_test_report.txt) -eq 0 ]
 then
   cat $ARTIFACTS_PATH/liquibase_yugabytedb_test_report.txt
 fi
-if [ $(grep -c "BUILD SUCCESS" $ARTIFACTS_PATH/liquibase_foundational_test_report.txt) -eq 0 ]
-then
-  cat $ARTIFACTS_PATH/liquibase_foundational_test_report.txt
-fi
-if [ $(grep -c "BUILD SUCCESS" $ARTIFACTS_PATH/liquibase_advanced_test_report.txt) -eq 0 ]
-then
-  cat $ARTIFACTS_PATH/liquibase_advanced_test_report.txt
-fi
+
+./generate-report.sh
 
 ! grep "BUILD FAILURE" $ARTIFACTS_PATH/liquibase_yugabytedb_test_report.txt
-! grep "BUILD FAILURE" $ARTIFACTS_PATH/liquibase_foundational_test_report.txt
-! grep "BUILD FAILURE" $ARTIFACTS_PATH/liquibase_advanced_test_report.txt
