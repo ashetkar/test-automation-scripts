eddca0866ee352e3631bb53ae52683edbde4db06
Amogh Shetkar <ashetkar@yugabyte.com>
2024-11-21 13:26:20 +0000

    Generate json load for Flyway plugin tests

--- flyway/run-app.sh
+++ flyway/run-app.sh
@@ -2,6 +2,8 @@
 set -e
 
 DIR="flyway-tests"
+REPORT_FILE="$WORKSPACE/artifacts/test_report_jdbc_ysql.json"
+
 if [ -d "$DIR" ]; then
  echo "$DIR repository is already present"
 else
@@ -16,6 +18,47 @@ git pull
 echo "Building and running the tests..."
 
 export YBDB_PATH=$YUGABYTE_HOME_DIRECTORY
+export JAVA_HOME=/usr/lib/jvm/zulu-17.jdk
 
-JAVA_HOME=/usr/lib/jvm/zulu-17.jdk mvn clean test
+# Function to run individual test cases and capture their results
+run_test() {
+    local test_name=$1
+    local script_name=$2
+
+    # Run the specific test case and capture errors
+    local tname="com.yugabyte.${script_name}#${test_name}"
+    echo "Running ${tname}..."
+    mvn test -Dtest=${tname} 2>&1 | tee ${test_name}.log
+    if ! grep "BUILD SUCCESS" ${test_name}.log; then
+      # Get the lines between 'FAILURE!' and 'BUILD FAILURE' which is the stack trace and replace new lines with '\n'
+      sed -n '/FAILURE!/,/BUILD FAILURE/{/FAILURE!/b;/BUILD FAILURE/b;p}' ${test_name}.log | awk '{printf "%s\\n", $0}' > stack4json.log
+      echo "{ \"test_name\": \"$test_name\", \"script_name\": \"$script_name\", \"result\": \"FAILED\", \"error_stack\": \"$(cat stack4json.log)\" }," >> temp_report.json
+      RESULT=1
+    else
+      echo "{ \"test_name\": \"$test_name\", \"script_name\": \"$script_name\", \"result\": \"PASSED\", \"error_stack\": \"\" }," >> temp_report.json
+    fi
+}
+
+mvn clean compile --no-transfer-progress
+
+echo "[" > temp_report.json
+
+run_test "baselineTest" "TestBaseline"
+run_test "dlabsTest" "TestBaseline"
+run_test "advisoryLockTest" "TestYBLocking"
+
+sed -i '$ s/,$//' temp_report.json # Remove trailing comma from the last JSON object
+echo "]" >> temp_report.json
+
+# Move the temporary report to the final report file
+mv temp_report.json "$REPORT_FILE"
+
+# Display the JSON report
+echo "TEST REPORT -------------------------"
+cat "$REPORT_FILE"
+
+readlink -f "$REPORT_FILE"
+
+exit $RESULT
+
 
