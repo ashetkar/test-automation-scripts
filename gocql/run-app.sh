b767a7281987ae9250445ab241c4774973cb0185
Amogh Shetkar <ashetkar@users.noreply.github.com>
2025-01-17 13:08:57 +0530

    Generate Gocql test report for Yugaboard (#31)
    * Enable yugaboard report for gocql tests and include read-replica tests for ruby-pg

--- gocql/run-app.sh
+++ gocql/run-app.sh
@@ -2,6 +2,8 @@
 set -e
 
 DIR="gocql"
+REPORT_FILE="$WORKSPACE/artifacts/test_report_gocql.json"
+
 if [ -d "$DIR" ]; then
  echo "gocql repository is already present"
  cd gocql
@@ -17,4 +19,34 @@ echo "Running tests"
 
 go clean -testcache
 
-go test -v
+# Run the specific test case and capture errors
+echo "Running gocql tests..."
+go test -v 2>&1 | tee gocql-tests.log
+
+echo "[" >> temp_report.json
+
+if grep "FAIL:" "gocql-tests.log"; then
+  # Get the lines with 'FAIL'
+  grep -B 1 "FAIL:" gocql-tests.log > stack4json.log
+  # test_name=`sed -n '/^.*FAIL:\s\+\(\w\+\).*$/s//\1/p' gocql-tests.log`
+  python $WORKSPACE/integrations/utils/create_json.py --test_name "NA" --script_name "gocql-test" --result FAILED --file_path stack4json.log >> temp_report.json
+  RESULT=1
+else
+  python $WORKSPACE/integrations/utils/create_json.py --test_name "NA" --script_name "gocql-test" --result PASSED >> temp_report.json
+fi
+
+sed -i '$ s/,$//' temp_report.json # Remove trailing comma from the last JSON object
+echo "]" >> temp_report.json
+sed -i 's/\t/    /g' temp_report.json # Replace tabs with spaces
+
+# Move the temporary report to the final report file
+mv temp_report.json "$REPORT_FILE"
+
+# Display the JSON report
+echo "TEST REPORT -------------------------"
+cat "$REPORT_FILE"
+
+readlink -f "$REPORT_FILE"
+
+exit $RESULT
+
